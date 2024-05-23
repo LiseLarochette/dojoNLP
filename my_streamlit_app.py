1d36502cc0b79d549a3ed17a4fbb30cafe484b64
Lise LAROCHETTE <168559876+LiseLarochette@users.noreply.github.com>
2024-05-23 11:05:59 +0200

    Update my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -2,50 +2,89 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime
 
-st.title('Application de Lise LAROCHETTE')
+st.title('DOJO NLP')
 
+phrase = input("Please insert a sentence:")
+
+# Bouton pour générer un match idéal après soumission des informations
+if st.button("Stemmer"):
+    st.success(stemming(phrase))
+
+
+if st.button("lemmatizing"):
+    st.success(lemmatized(phrase))
+
+
+if st.button("TF - IDF"):
+    st.success(tf_idf(phrase))
+
+if st.button("Word Cloud"):
+    st.success(wordcloud(phrase))
+  
+
+
+def stemming(phrase):
+    from nltk.stem import SnowballStemmer
+    # Tokenize the sentence and remove stopwords
+    tokens_clean = []
+    for words in nltk.word_tokenize(sentence.lower()):
+      if words not in nltk.corpus.stopwords.words("english"):
+        tokens_clean.append(words)
+    # Remove punctuation
+    tokens_clean = [word for word in tokens_clean if word.isalnum()]
+    # Initialize the SnowballStemmer
+    stem_en = SnowballStemmer("english")
+    # Stem the words
+    stemmed_words = [stem_en.stem(word) for word in tokens_clean]
+    # Display the results in a message box
+    print("Stemming", f"Stemmed words: {stemmed_words}")
+
+def lemmatized(phrase):
+    import spacy
+    # Import des règles anglaises
+    nlp = spacy.load('en_core_web_sm')
+    #remove stopwords
+    tokens_clean = []
+    for words in nltk.word_tokenize(phrase.lower()):
+      if words not in nltk.corpus.stopwords.words("english"):
+        tokens_clean.append(words)
+    # Remove punctuation
+        tokens_clean = [word for word in tokens_clean if word.isalnum()]
+    # spacy découpe automatiquement en tokens avec cette syntaxe :
+    lemmed_words = [nlp(word) for word in tokens_clean]
+    print(" Lemmatizing:", f"Lemmatized words: {lemmed_words}")
+
+#TF IDF
 st.write("Dataframe")
+def tf_idf(phrase):
+    from sklearn.feature_extraction.text import TfidfVectorizer
+    # Créer un TfidfVectorizer
+    vectorizer = TfidfVectorizer()
+    # Ajuster le vecteur TF-IDF
+    # Transformer la phrase
+    tfidf_matrix = vectorizer.fit_transform([phrase])
+    # Convertir le résultat en df pour une meilleure visualisation
+    df_tfidf = pd.DataFrame(tfidf_matrix.toarray(), columns=vectorizer.get_feature_names_out())
+    # Afficher le df TF-IDF
+    print(df_tfidf)
 
-link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
-df_voitures = pd.read_csv(link)
-
-#afficher la DF
-df_voitures
-
-
-#créer un filtre
-
-# Créer un bouton pour filtrer par continent "Europe."
-if st.button("Filtrer par continent 'Europe.'"):
-    df_filtered = df_voitures[df_voitures['continent'].str.contains('Europe')]
-    # Afficher le DataFrame filtré
-    st.write("DataFrame après le filtre :")
-    st.write(df_filtered)
-
-if st.button("Filtrer par continent 'US.'"):
-    df_filtered = df_voitures[df_voitures['continent'].str.contains('US')]
-    # Afficher le DataFrame filtré
-    st.write("DataFrame après le filtre :")
-    st.write(df_filtered)
-
-if st.button("Filtrer par continent 'Japan.'"):
-    df_filtered = df_voitures[df_voitures['continent'].str.contains('Japan')]
-    # Afficher le DataFrame filtré
-    st.write("DataFrame après le filtre :")
-    st.write(df_filtered)
-
-
-#afficher un graphique en bar
-st.title("Puissance des voitures (cylindres) par nationalité")
-st.bar_chart(data=df_voitures, x='continent', y='cylinders')
-st.write("On remarque que les voitures américaines sont les plus puissantes.")
-
-#afficher un graphique en courbe
-import numpy as np
-
-st.title("Consommation des voitures en fonction de la puissance (cylindres)")
-st.line_chart(data=df_voitures, x= 'weightlbs', y='mpg')
-st.write("On remarque que les voitures qui ont un poids entre 2000 et 2800 lbs consomment le plus.")
-
-
-
+def wordcloud(phrase):
+  import re
+  import nltk
+  from nltk.corpus import stopwords
+  from wordcloud import WordCloud
+  import matplotlib.pyplot as plt
+  # Télécharger les stopwords
+  nltk.download('stopwords')
+  # Suppression de la ponctuation
+  clean_sentence = re.sub(r'[^\w\s]', '', phrase)
+  # Suppression des stopwords
+  stop_words = set(stopwords.words('french'))
+  clean_sentence = ' '.join([word for word in clean_sentence.split() if word.lower() not in stop_words])
+  # Créer un objet WordCloud
+  wordcloud = WordCloud(width=800, height=400, background_color='white').generate(clean_sentence)
+  # Afficher le word cloud
+  plt.figure(figsize=(10, 5))
+  plt.imshow(wordcloud, interpolation='bilinear')
+  plt.axis('off')
+  plt.show()
