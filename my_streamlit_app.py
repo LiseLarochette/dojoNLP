4f433b8d0c79332f5b0cbb062d65ea8cd6f75b0e
Lise LAROCHETTE <168559876+LiseLarochette@users.noreply.github.com>
2024-05-23 11:10:45 +0200

    Update my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -1,90 +1,79 @@
 import streamlit as st
 import pandas as pd
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import spacy
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+# Télécharger les stopwords
+nltk.download('stopwords')
+
+# Import des règles anglaises pour Spacy
+nlp = spacy.load('en_core_web_sm')
 
 st.title('DOJO NLP')
 
-phrase = input("Please insert a sentence:")
-
-# Bouton pour générer un match idéal après soumission des informations
-if st.button("Stemmer"):
-    st.success(stemming(phrase))
-
-
-if st.button("lemmatizing"):
-    st.success(lemmatized(phrase))
-
-
-if st.button("TF - IDF"):
-    st.success(tf_idf(phrase))
-
-if st.button("Word Cloud"):
-    st.success(wordcloud(phrase))
-  
-
+phrase = st.text_input("Please insert a sentence:")
 
 def stemming(phrase):
-    from nltk.stem import SnowballStemmer
     # Tokenize the sentence and remove stopwords
-    tokens_clean = []
-    for words in nltk.word_tokenize(sentence.lower()):
-      if words not in nltk.corpus.stopwords.words("english"):
-        tokens_clean.append(words)
+    tokens_clean = [word for word in nltk.word_tokenize(phrase.lower()) if word not in stopwords.words("english")]
     # Remove punctuation
     tokens_clean = [word for word in tokens_clean if word.isalnum()]
     # Initialize the SnowballStemmer
     stem_en = SnowballStemmer("english")
     # Stem the words
     stemmed_words = [stem_en.stem(word) for word in tokens_clean]
-    # Display the results in a message box
-    print("Stemming", f"Stemmed words: {stemmed_words}")
+    return " ".join(stemmed_words)
 
-def lemmatized(phrase):
-    import spacy
-    # Import des règles anglaises
-    nlp = spacy.load('en_core_web_sm')
-    #remove stopwords
-    tokens_clean = []
-    for words in nltk.word_tokenize(phrase.lower()):
-      if words not in nltk.corpus.stopwords.words("english"):
-        tokens_clean.append(words)
+def lemmatizing(phrase):
+    # Tokenize the sentence and remove stopwords
+    tokens_clean = [word for word in nltk.word_tokenize(phrase.lower()) if word not in stopwords.words("english")]
     # Remove punctuation
-        tokens_clean = [word for word in tokens_clean if word.isalnum()]
-    # spacy découpe automatiquement en tokens avec cette syntaxe :
-    lemmed_words = [nlp(word) for word in tokens_clean]
-    print(" Lemmatizing:", f"Lemmatized words: {lemmed_words}")
+    tokens_clean = [word for word in tokens_clean if word.isalnum()]
+    # Lemmatize the words
+    lemmed_words = [token.lemma_ for token in nlp(" ".join(tokens_clean))]
+    return " ".join(lemmed_words)
 
-#TF IDF
-st.write("Dataframe")
 def tf_idf(phrase):
-    from sklearn.feature_extraction.text import TfidfVectorizer
     # Créer un TfidfVectorizer
     vectorizer = TfidfVectorizer()
-    # Ajuster le vecteur TF-IDF
-    # Transformer la phrase
+    # Ajuster le vecteur TF-IDF et transformer la phrase
     tfidf_matrix = vectorizer.fit_transform([phrase])
-    # Convertir le résultat en df pour une meilleure visualisation
+    # Convertir le résultat en DataFrame pour une meilleure visualisation
     df_tfidf = pd.DataFrame(tfidf_matrix.toarray(), columns=vectorizer.get_feature_names_out())
-    # Afficher le df TF-IDF
-    print(df_tfidf)
+    return df_tfidf
 
-def wordcloud(phrase):
-  import re
-  import nltk
-  from nltk.corpus import stopwords
-  from wordcloud import WordCloud
-  import matplotlib.pyplot as plt
-  # Télécharger les stopwords
-  nltk.download('stopwords')
-  # Suppression de la ponctuation
-  clean_sentence = re.sub(r'[^\w\s]', '', phrase)
-  # Suppression des stopwords
-  stop_words = set(stopwords.words('french'))
-  clean_sentence = ' '.join([word for word in clean_sentence.split() if word.lower() not in stop_words])
-  # Créer un objet WordCloud
-  wordcloud = WordCloud(width=800, height=400, background_color='white').generate(clean_sentence)
-  # Afficher le word cloud
-  plt.figure(figsize=(10, 5))
-  plt.imshow(wordcloud, interpolation='bilinear')
-  plt.axis('off')
-  plt.show()
+def generate_wordcloud(phrase):
+    # Suppression de la ponctuation
+    clean_sentence = re.sub(r'[^\w\s]', '', phrase)
+    # Suppression des stopwords
+    stop_words = set(stopwords.words('french'))
+    clean_sentence = ' '.join([word for word in clean_sentence.split() if word.lower() not in stop_words])
+    # Créer un objet WordCloud
+    wordcloud = WordCloud(width=800, height=400, background_color='white').generate(clean_sentence)
+    # Afficher le word cloud
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation='bilinear')
+    plt.axis('off')
+    st.pyplot(plt)
+
+if st.button("Stemmer"):
+    st.write("Stemmed Sentence:")
+    st.success(stemming(phrase))
+
+if st.button("Lemmatizing"):
+    st.write("Lemmatized Sentence:")
+    st.success(lemmatizing(phrase))
+
+if st.button("TF - IDF"):
+    st.write("TF-IDF DataFrame:")
+    st.dataframe(tf_idf(phrase))
+
+if st.button("Word Cloud"):
+    st.write("Word Cloud:")
+    generate_wordcloud(phrase)
