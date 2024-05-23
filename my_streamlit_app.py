8e7b83338b320b19d685017b59acbb742eb137d3
Lise LAROCHETTE <168559876+LiseLarochette@users.noreply.github.com>
2024-05-23 11:19:02 +0200

    Update my_streamlit_app.py

--- my_streamlit_app.py
+++ my_streamlit_app.py
@@ -2,8 +2,9 @@ import streamlit as st
 import pandas as pd
 import re
 
+st.title('DOJO NLP')
 
-
+phrase = input("Please insert a sentence:")
 
 # Ajouter des boutons pour chaque fonctionnalité
 if st.button("Stemmer"):
